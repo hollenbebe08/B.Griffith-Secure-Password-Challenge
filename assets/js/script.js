7513a01b5431181cf7fa3c9065e3afd0505596ff
Betsy <betsy.griffith12@gmail.com>
2022-05-18 15:19:51 -0400

    added functionality to generate text in the password text area by adding getElementbyId

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,18 @@
 // Assignment code here
+document.getElementById("generate"). addEventListener("click", writePassword);
+function writePassword() {
+  document.getElementById("password").innerHTML = "password text";
+};
 
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+// var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword);
