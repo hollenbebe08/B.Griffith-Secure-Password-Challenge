01f2f136676bdc6b060c9ca9561bacd26221a3df
Betsy <betsy.griffith12@gmail.com>
2022-05-21 20:06:08 -0400

    added confirmations for criteria and a new function to generate the password

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,71 +5,67 @@ var specialCharactersArray = ["!", "#", "$", "%", "&", ")", "(", "*", "+", "-", 
 var numericalArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var overallArray =[upperCaseArray, lowerCaseArray, specialCharactersArray, numericalArray];
 
+var upperCaseConfirmation;
+var lowerCaseConfirmation;
+var specialCharactersConfirmation;
+var numericConfirmation;
+var passwordLengthInt;
+
 // generatePassword function
-var generatePassword = function () {
+var getConfirmations = function () {
   //confirmation to generate password
   window.confirm("Please choose which criteria you would like to include in your password by following the prompts.");
 
   //prompt for upperCase Array
-  var upperCaseArray = window.prompt("Would you like to have Uppercase letters in your password?");
-  if (upperCaseArray === "NO" || upperCaseArray === "no") {
-    window.alert("Alright. No Uppercase letters will be included.");
-  } else {(upperCaseArray === "YES" || upperCaseArray === "yes");
-    window.alert("Great! Uppercase letters will be included in your password.");
-  }
+  upperCaseConfirmation = window.confirm("Would you like to have Uppercase letters in your password?");
+  console.log(upperCaseConfirmation);
+
 
   //prompt for lowerCase Array
-  var lowerCaseArray = window.prompt("Would you like to have Lowercase letters in your password?");
-  if (lowerCaseArray === "NO" || lowerCaseArray === "no") {
-    window.alert("Alright. No Lowercase letters will be included.");
-  } else {(lowerCaseArray === "YES" || lowerCaseArray === "yes");
-    window.alert("Ok! Lowercase letters will be included in your password.");
-  }
+  lowerCaseConfirmation= window.confirm("Would you like to have Lowercase letters in your password?");
 
   //prompt for special characters
-  var specialCharactersArray = window.prompt("Would you like to have special characters in your password?");
-  if (specialCharactersArray === "NO" || specialCharactersArray === "no") {
-    window.alert("Sounds good. No special characters will be included.");
-  } else {(specialCharactersArray === "YES" || specialCharactersArray === "yes");
-    window.alert("Can do! Special characters will be included in your password.");
-  }
+  specialCharactersConfirmation = window.confirm("Would you like to have special characters in your password?");
 
   //prompt for numeric characters
-  var numericalArray = window.prompt("Would you like to have your password include numbers?");
-  if (numericalArray === "NO" || numericalArray === "no") {
-    window.alert("No problem. No numbers will be included.");
-  } else {(numericalArray === "YES" || numericalArray === "yes");
-    window.alert("Sounds like a plan! Numbers will be included in your password.");
-  }
+  numericConfirmation = window.confirm("Would you like to have your password include numbers?");
+
 
   //Prompt for password length
-    var passwordLengthString = window.prompt("Please type a number between 8 -128 to indicate how long you'd like your password to be.");
-    var passwordLengthInt = parseInt(passwordLengthString)
-    var x = 8;
-    var y = 128;
-  if(passwordLengthInt < x || passwordLengthInt > y) {
-      window.alert("Your password must be between 8 -128 characters in length. Try again!");
-  return passwordLengthString;
-  } else {(passwordLengthInt > x || passwordLengthInt < y);
-      window.alert("Great! Click the 'Generate Password' button to generate your password.");
-  }
+  var passwordLengthString = window.prompt("Please type a number between 8 -128 to indicate how long you'd like your password to be.");
+//  for (let i=0; i < 10; i++) {
+//   console.log(Math.floor(Math.random() * 10));
+//  };
+  // Password length variables
+  passwordLengthInt = parseInt(passwordLengthString);
+
 }; //end of generate password function
 
+var generatePassword= function(upperCaseConfirmation, lowerCaseConfirmation, specialCharactersConfirmation, numericConfirmation, passwordLengthInt) {
+  console.log(upperCaseConfirmation, lowerCaseConfirmation, specialCharactersConfirmation, numericConfirmation, passwordLengthInt);
+  // var yourPassword= [];
+};
+
 //call to action to generate password
-generatePassword();
+getConfirmations();
+generatePassword(upperCaseConfirmation, lowerCaseConfirmation, specialCharactersConfirmation, numericConfirmation, passwordLengthInt);
+
+//write password function
+// var writePassword = function() {
+
+// };
+
 
 
 //Event listener to generate password
-// document.getElementById("generate"). addEventListener("click", writePassword);
-// function writePassword() {
-//   document.getElementById("password").innerHTML = "password text";
-// };
+document.getElementById("generate"). addEventListener("click", writePassword);
+function writePassword() {
+  document.getElementById("password").innerHTML = "password text";
+};
 
 // var generateBtn = document.querySelector("#generate");
 
 // var passwordText = document.querySelector("#password");
 
 //   passwordText.value = password;
-
-//then add the created arrays to the overallarray
 
